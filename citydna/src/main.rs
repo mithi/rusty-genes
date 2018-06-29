8c37b340806b1509fffe5a379125a3aad2f89c37
Mithi Sevilla <mithi.sevilla@gmail.com>
2018-06-30 03:22:25 +0800

    feat: Get filepaths from command line

--- citydna/src/main.rs
+++ citydna/src/main.rs
@@ -1,25 +1,64 @@
 extern crate citydna;
-extern crate rand;
-
-use citydna::Simulation;
 use citydna::city;
+// use citydna::examples;
+use citydna::{City, Simulation};
+use citydna::helper;
+use std::process;
+use std::env;
 
 fn main() {
 
-    let iterations: usize = 1000;
-    let population_size: usize = 100; 
-    let crossover_probability = 0.6;
-    let mutation_probability = 0.001; 
+    // examples::example_one();
+    // examples::example_two();
 
-    let cities = city::test_cities(); // vec of 9 cities
+    // ----------------------
+    // PARSE ARGUMENTS
+    // ----------------------
+    let mut args = env::args().skip(1);
+
+    let specs_filename = args.next()
+        .unwrap_or_else( || {
+            eprintln!("Please specify filename with simulation specifications. \
+            \n USAGE: cargo run ./specs.csv /cities.csv > output.csv");
+            process::exit(1)
+            }
+        );
+
+    let city_filename = args.next()
+        .unwrap_or_else( || {
+            eprintln!("Please specify filename  with cities. \
+            \n USAGE: cargo run ./specs.csv /cities.csv > output.csv");
+            process::exit(1)
+            }
+        );
+
+
+    // ----------------------
+    // EXTRACT SPECS AND DATA
+    // ----------------------
+    let contents = helper::read_file(&specs_filename);
+    let (debug_level, 
+         skip,
+         iterations, 
+         population_size,
+         crossover_probability,
+         mutation_probability) = helper::parse_specs(&contents);
+
+    let contents = helper::read_file(&city_filename);
+    let cities: Vec<City> = city::string_to_cities(&contents);
+    
+
+    // ----------------------
+    // RUN SIMULATION
+    // ----------------------
 
     let mut sim = Simulation::new(
         iterations,
         crossover_probability, 
         mutation_probability, 
         population_size,
-        cities);
+        cities
+    );
 
-    sim.run(2);
+    sim.run(debug_level, skip);
 }
-
